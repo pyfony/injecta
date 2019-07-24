9020d10e5612c3dbe9722f341f21b1b82d170186
Jiri Koutny <jirkakoutny@gmail.com>
2019-07-24 09:27:06 +0200

    Better error messages
    To help the user to understand the real problem

--- src/Injecta/Autowiring/ArgumentResolver.py
+++ src/Injecta/Autowiring/ArgumentResolver.py
@@ -19,8 +19,14 @@ class ArgumentResolver:
             raise Exception('Cannot resolve argument {} for service {}'.format(argumentName, definition.getName()))
 
         if classFqn not in classes:
-            raise Exception('Service not found for {} used in {}'.format(classFqn, definition.getName()))
-            
+            className = classFqn[classFqn.rfind('.') + 1:]
+
+            if className in classes:
+                affectedServiceName = classes[className][0]
+                raise Exception('Setting "class: {}" for service "{}" is likely missing'.format(classFqn, affectedServiceName))
+            else:
+                raise Exception('Service not found for {} used in {}'.format(classFqn, definition.getName()))
+
         if len(classes[classFqn]) > 1:
             serviceNames = ', '.join(classes[classFqn])
             raise Exception('Multiple services of {} defined ({}), class used in {}'.format(classFqn, serviceNames, definition.getName()))
