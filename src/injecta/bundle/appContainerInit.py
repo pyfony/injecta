8a0ee784558229ada6d8a1e859dc8dc8332191a7
Jiri Koutny <jirkakoutny@gmail.com>
2020-01-23 17:33:48 +0100

    User current directory as base directory
    For fetching app's pyproject.toml

--- src/injecta/bundle/appContainerInit.py
+++ src/injecta/bundle/appContainerInit.py
@@ -5,10 +5,9 @@ from injecta.container.ContainerInterface import ContainerInterface
 from injecta.bundle.appConfigLoader import loadAppConfig
 
 def initAppContainer(appEnv: str) -> ContainerInterface:
-    currentDir = os.path.dirname(os.path.abspath(__file__))
-    projectRoot = Path(currentDir).parent.parent.parent.parent.parent
+    workingDir = Path(os.getcwd())
 
-    appConfig = loadAppConfig(projectRoot.joinpath('pyproject.toml'))
+    appConfig = loadAppConfig(workingDir.joinpath('pyproject.toml'))
     containerInitConfig = appConfig['container-init']
 
     initContainerFunction = loadClass(containerInitConfig[0], containerInitConfig[1])
