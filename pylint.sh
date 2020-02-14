f0225cd656c822031b575a50261745b62559fa81
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-14 07:03:50 +0100

    Pylint call simplified

--- pylint.sh
+++ pylint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-find src -iname "*.py" -not -name "*Test.py" | xargs pylint --rcfile=.pylintrc \
+pylint \
+--ignore-patterns=".+Test\.py" \
+--rcfile=.pylintrc \
 --class-naming-style=PascalCase \
 --module-rgx=".+" \
 --function-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
@@ -9,4 +11,5 @@ find src -iname "*.py" -not -name "*Test.py" | xargs pylint --rcfile=.pylintrc \
 --argument-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
 --variable-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
 --class-attribute-rgx="^[_]{0,2}[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
---const-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$"
+--const-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
+src
