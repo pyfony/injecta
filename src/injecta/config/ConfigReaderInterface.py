66822f7b5248f04a820a36fbf6371b9f2a990d4f
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-16 18:38:43 +0100

    Interface implementation fixed

--- src/injecta/config/ConfigReaderInterface.py
+++ src/injecta/config/ConfigReaderInterface.py
@@ -1,6 +1,7 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class ConfigReaderInterface(ABC):
 
+    @abstractmethod
     def read(self, configPath: str) -> dict:
         pass
--- src/injecta/service/argument/ArgumentInterface.py
+++ src/injecta/service/argument/ArgumentInterface.py
@@ -1,10 +1,12 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from injecta.service.class_.ConstructorArgument import ConstructorArgument
 
 class ArgumentInterface(ABC):
 
+    @abstractmethod
     def getStringValue(self):
         pass
 
+    @abstractmethod
     def checkTypeMatchesDefinition(self, constructorArgument: ConstructorArgument, services2Classes: dict):
         pass
