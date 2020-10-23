fc31cf6b1fd836e52f553e9e71d1c8df4feb9bc8
Jiri Koutny <jirkakoutny@gmail.com>
2020-10-23 07:45:17 +0200

    ModuleNotFound error message contain class name
    To simplify debugging

--- src/injecta/dtype/classLoader.py
+++ src/injecta/dtype/classLoader.py
@@ -1,5 +1,9 @@
 from importlib import import_module
 
 def loadClass(moduleName, className):
-    module = import_module(moduleName)
+    try:
+        module = import_module(moduleName)
+    except ModuleNotFoundError:
+        raise Exception(f'No module named {moduleName} (class {className})')
+
     return getattr(module, className)
