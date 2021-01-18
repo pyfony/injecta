4f7d8644b354099c5c4c75b2e00cb0294d90b444
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-18 14:56:11 +0100

    ContainerBuild building logic separated

--- src/injecta/container/ContainerBuilder.py
+++ src/injecta/container/ContainerBuilder.py
@@ -1,10 +1,12 @@
 from typing import List
+from box import Box
 from injecta.autowiring.ArgumentResolver import ArgumentResolver
 from injecta.autowiring.ArgumentsAutowirer import ArgumentsAutowirer
 from injecta.autowiring.AutowiringCompilerPass import AutowiringCompilerPass
 from injecta.container.Hooks import Hooks
 from injecta.generator.Tag2ServicesPreparer import Tag2ServicesPreparer
 from injecta.container.ContainerBuild import ContainerBuild
+from injecta.service.Service import Service
 from injecta.service.ServicesPreparer import ServicesPreparer
 from injecta.service.DTypeResolver import DTypeResolver
 from injecta.parameter.ParametersParser import ParametersParser
@@ -46,17 +48,10 @@ class ContainerBuilder:
         services = self.__servicesPreparer.prepare(rawConfig['services'])
         services = hooks.servicesPrepared(services)
 
-        classes2Services = self.__classes2ServicesBuilder.build(services)
-        services2Classes = dict(map(lambda service: (service.name, service.class_), services))
-
-        resolvedServices = list(map(lambda service: self.__servicesResolver.resolve(service, services2Classes), services)) # type: List[ResolvedService]
-
-        tag2Services = self.__tag2ServicesPreparer.prepare(services)
-
         parameters = self.__parametersParser.parse(rawConfig['parameters'], hooks.getCustomParameters())
         parameters = hooks.parametersParsed(parameters)
 
-        containerBuild = ContainerBuild(parameters, resolvedServices, classes2Services, tag2Services)
+        containerBuild = self._build(parameters, services)
 
         for compilerPass in self.__defaultCompilerPasses:
             compilerPass.process(containerBuild)
@@ -64,3 +59,13 @@ class ContainerBuilder:
         hooks.containerBuildReady(containerBuild)
 
         return containerBuild
+
+    def _build(self, parameters: Box, services: List[Service]):
+        classes2Services = self.__classes2ServicesBuilder.build(services)
+        services2Classes = dict(map(lambda service: (service.name, service.class_), services))
+
+        resolvedServices: List[ResolvedService] = [self.__servicesResolver.resolve(service, services2Classes) for service in services]
+
+        tag2Services = self.__tag2ServicesPreparer.prepare(services)
+
+        return ContainerBuild(parameters, resolvedServices, classes2Services, tag2Services)
