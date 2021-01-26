0bd97e15e22cdcc23a7961a941de5d5510aa57cf
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-26 10:13:03 +0100

    Loading attributes from string support
    * "foo.bar.mymodule:myfunction"
    * "foo.bar.mymodule:MyClass.create"

--- src/injecta/module/attributeLoader.py
+++ src/injecta/module/attributeLoader.py
@@ -7,3 +7,15 @@ def load(moduleName, attributeName):
         raise Exception(f'No module named {moduleName} (attribute: {attributeName})')
 
     return getattr(module, attributeName)
+
+def loadFromString(val):
+    moduleName, classAndMethod = val.split(':')
+
+    if '.' not in classAndMethod:
+        return load(moduleName, classAndMethod)
+
+    className, methodName = classAndMethod.split('.')
+
+    class_ = load(moduleName, className) # pylint: disable = invalid-name
+
+    return getattr(class_, methodName)
