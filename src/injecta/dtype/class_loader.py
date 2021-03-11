ea6336d4a273c40a59323256a2f92b5993f05ebf
Jiri Koutny <jirkakoutny@gmail.com>
2021-03-11 09:29:32 +0100

    deprecated function removed

--- src/injecta/dtype/class_loader.py
+++
@@ -1,5 +0,0 @@
-from injecta.module import attribute_loader
-
-# deprecated, use loadAttribute instead
-def load_class(module_name, class_name):  # noqa: 5302
-    return attribute_loader.load(module_name, class_name)
