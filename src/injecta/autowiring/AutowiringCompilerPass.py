6f229df50357efab0bc69fb371c9231acee207b2
kutny <jirkakoutny@gmail.com>
2021-05-24 16:39:33 +0200

    Better check for service argument count

--- src/injecta/autowiring/AutowiringCompilerPass.py
+++ src/injecta/autowiring/AutowiringCompilerPass.py
@@ -9,13 +9,23 @@ class AutowiringCompilerPass(CompilerPassInterface):
         self.__arguments_autowirer = arguments_autowirer
 
     def process(self, container_build: ContainerBuild):
+        to_autowire = []
+
         def should_autowire(resolved_service: ResolvedService):
             return resolved_service.service.autowire is True and resolved_service.resolved_arguments
 
-        services_for_autowiring = list(filter(should_autowire, container_build.resolved_services))
+        for resolved_service in container_build.resolved_services:
+            if should_autowire(resolved_service):
+                to_autowire.append(resolved_service)
+            else:
+                self.__check_skipped(resolved_service)
 
-        for resolved_service in services_for_autowiring:
+        for resolved_service in to_autowire:
             self.__autowire(resolved_service, container_build.classes2_services)
+
+    def __check_skipped(self, resolved_service: ResolvedService):
+        if len(resolved_service.service.arguments) < len(resolved_service.resolved_arguments):
+            raise Exception(f'Too few arguments given for service "{resolved_service.service.name}"')
 
     def __autowire(self, resolved_service: ResolvedService, classes2_services: dict):
         resolved_arguments = self.__arguments_autowirer.autowire(
